9687a606acc98026ba907c0f60a83df41c99cfed
Nir Kigelman <kigelman.nir@gmail.com>
2019-09-09 22:53:55 +0300

    MongoAlchemy Added.

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask.ext.mongoalchemy import MongoAlchemy
+
+db = MongoAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    db.init_app(app)
     
     return app
