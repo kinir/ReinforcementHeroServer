bdc15fceabe62f115ef51d3f6e810e077706a253
Nir Kigelman <kigelman.nir@gmail.com>
2019-09-19 16:12:47 +0300

    Implemented gunicorn + structure change.

--- server/web/app.py
+++ server/web/app.py
@@ -2,6 +2,7 @@ from flask_restful import Api
 
 from . import init_app
 from .controllers.submission_controller import Submission
+from .controllers.test_controller import Test
 
 # Init app and db
 app = init_app()
@@ -10,3 +11,6 @@ app.app_context().push()
 # Add resources and routes
 api = Api(app)
 api.add_resource(Submission, "/api/submit")
+
+# Test resource
+api.add_resource(Test, "/api/test")
