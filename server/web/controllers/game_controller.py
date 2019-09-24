39b847766cc675352825ddbf18d5e544d3fcee46
Avocado-BHB-User6 <Avocado-BHB-User6@yatzma.onmicrosoft.com>
2019-09-24 20:46:08 +0300

    Add find all games function

--- server/web/controllers/game_controller.py
+++ server/web/controllers/game_controller.py
@@ -20,7 +20,10 @@ class Game(Resource):
 
         return True
 
-    def get(self, game_id):
-        game_properties = service.find_game(game_id)
-
-        return jsonify(game_properties)
+    def get(self, game_id=None):
+        if(game_id is None):
+            all_games = service.find_all_games()
+            return jsonify(all_games)
+        else:
+            game_properties = service.find_game(game_id)
+            return jsonify(game_properties)
