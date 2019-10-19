430450820660001e416481574342237cafa01240
Nir Kigelman <kigelman.nir@gmail.com>
2019-10-20 00:11:04 +0300

    All db related stuff moved to database.py.

--- server/web/database.py
+++ server/web/database.py
@@ -1,1 +1,39 @@
 from . import db
+from .models.submission_model import Submission
+
+from bson.objectid import ObjectId
+
+def insert_one(collection, document):
+    db.db[collection].insert_one(document)
+
+def find_all_documents(collection, show_fields=None, hide_fields=None):
+    return db.db[collection].find({}, construct_projection(show_fields, hide_fields))
+
+def find_by_id(collection, id, show_fields=None, hide_fields=None):
+    query = {
+        "_id": ObjectId(id)
+    }
+
+    return db.db[collection].find_one(query, construct_projection(show_fields, hide_fields))
+
+def find_submissions_by_game(game_id, show_fields=None, hide_fields=None):
+    query = {
+        "game_id": game_id
+    }
+    
+    return db.db[Submission.collection].find(query, construct_projection(show_fields, hide_fields))
+    
+def find_submissions_by_student(student_id, show_fields=None, hide_fields=None):
+    query = {
+        "group_ids": { "$elemMatch": { "$eq": student_id } }
+    }
+
+    return db.db[Submission.collection].find(query, construct_projection(show_fields, hide_fields))
+
+def construct_projection(show_fields=None, hide_fields=None):
+    #if show_fields is None and hide_fields is None:
+    #    projection = {}
+    #else:
+    projection = { field_name: 1 if field_name in show_fields else 0 for field_name in show_fields + hide_fields }
+
+    return projection
