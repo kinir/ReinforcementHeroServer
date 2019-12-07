cf81c8298728fe384e56559f3ef660aeee86b0b5
Avocado-BHB-User6 <Avocado-BHB-User6@yatzma.onmicrosoft.com>
2019-12-07 23:10:51 +0200

    check

--- server/web/database.py
+++ server/web/database.py
@@ -62,11 +62,8 @@ def find_submissions_by_game(game_id, show_fields=None, hide_fields=None):
     
     return db.db[Submission.collection].find(query, construct_projection(show_fields, hide_fields))
     
-<<<<<<< HEAD
+# Find environment by game id
 def find_env_by_game(game_id, show_fields=None, hide_fields=None):
-=======
-def find_env_name_by_game_id(game_id, show_fields=None, hide_fields=None):
->>>>>>> 37bb70181f48cf5a40931d50a1311692efef8a4c
     query = [
         { "$match": { "_id": ObjectId(game_id) }},
         {
@@ -77,12 +74,6 @@ def find_env_name_by_game_id(game_id, show_fields=None, hide_fields=None):
                 "as" : "env"
             }
         },
-<<<<<<< HEAD
-=======
-        {
-            "$replaceRoot": { "newRoot": { "$mergeObjects": [ { "$arrayElemAt": [ "$env", 0 ] }, "$$ROOT" ] } }
-        },
->>>>>>> 37bb70181f48cf5a40931d50a1311692efef8a4c
         { "$project": construct_projection(show_fields, hide_fields) }
     ]
 
