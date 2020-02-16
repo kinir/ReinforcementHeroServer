f4839f1589d5c6c83b490508b4f1ede7efbf98d0
Nir Kigelman <nkigelman@nkigelman-ltm1.internal.salesforce.com>
2020-02-16 21:23:07 +0200

    Added traceback

--- server/web/error_handler.py
+++ server/web/error_handler.py
@@ -1,5 +1,8 @@
 from flask import jsonify
+import traceback
 
 def handle_exception(e):
     print(f"ERROR - { str(e) }")
+    print(repr(traceback.format_stack()))
+    
     return jsonify(error=str(e)), 500
